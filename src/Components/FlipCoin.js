c13dc4ce1d805a811d1b2978789d2262be82e315
tabishadnan <tabishadnan000@gmail.com>
2020-10-02 12:45:41 -0700

    add some functionality in flipcoin component

--- src/Components/FlipCoin.js
+++ src/Components/FlipCoin.js
@@ -13,20 +13,37 @@ class FlipCoin extends Component {
         isfilpStart : false,
         nFlips : 0,
         nHeads : 0,
-        nTails : 0
+        nTails : 0,
+        newCoin : null
     }
 
     handleFlip = () => {
+        
         this.setState({
-            isfilpStart : true
+            isfilpStart : true,
+            newCoin : this.props.coins[Math.floor(Math.random() * this.props.coins.length)]
         })
+
+        {this.state.isfilpStart && this.setState({
+            nFlips : this.state.nFlips + 1
+        })}
+
+        {this.state.isfilpStart && this.state.newCoin.name === "heads" && this.setState({
+            nHeads : this.state.nHeads + 1
+        })}
+
+        {this.state.isfilpStart && this.state.newCoin.name === "tails" && this.setState({
+            nTails : this.state.nTails + 1
+        })}
+
+
     }
 
     render() {
         return (
             <div>
                 <h1>Let's Flip A Coin</h1>
-                {this.state.isfilpStart && <div><img src={this.props.coins[0].imgSrc} alt={this.props.coins[0].name} /></div>}
+                {this.state.isfilpStart && <div><img src={this.state.newCoin.imgSrc} /></div>}
                 <button onClick={this.handleFlip}>Flip Me!</button>
                 <p>Out of {this.state.nFlips}, there have been {this.state.nHeads} heads and {this.state.nTails} tails</p>
             </div>
