231a65720a820e085f67f34aaaa7fd7deb6b7e87
tabishadnan <tabishadnan000@gmail.com>
2020-12-23 22:54:27 +0500

    Updates

---
+++ precache-manifest.cc76d28d570672b7cbc3156deaa126b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f652779405ae806722160b1ba51828",
+    "url": "/coin-flip-exercise/index.html"
+  },
+  {
+    "revision": "88819c94502ac0c81165",
+    "url": "/coin-flip-exercise/static/css/main.8b360bb4.chunk.css"
+  },
+  {
+    "revision": "904d8fa8640dac954046",
+    "url": "/coin-flip-exercise/static/js/2.c04ab6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coin-flip-exercise/static/js/2.c04ab6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88819c94502ac0c81165",
+    "url": "/coin-flip-exercise/static/js/main.11c1a871.chunk.js"
+  },
+  {
+    "revision": "f76986d458288bad1e13",
+    "url": "/coin-flip-exercise/static/js/runtime-main.4b1018d7.js"
+  }
+]);
